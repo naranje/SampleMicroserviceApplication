7085b91385ced13fb790d7359dc752ff3381209f
QUADRUS\mirann <mirann@quadrus.com>
2018-04-29 21:18:46 -0600

    cleanup

--- BookRequest/LibraryCatalog/ResiliencePolicies.cs
+++ BookRequest/LibraryCatalog/ResiliencePolicies.cs
@@ -1,48 +1,49 @@
 using System;
 using System.Threading.Tasks;
-using BookRequest.LibraryCatalog;
 using Polly;
-using Polly.CircuitBreaker;
 
-internal class ResiliencePolicies
+namespace BookRequest.LibraryCatalog
 {
-    // Break the circuit after the specified number of consecutive exceptions
-    // and keep circuit broken for the specified duration,
-    // calling an action on change of circuit state.
-    private static readonly Action<Exception, TimeSpan> OnBreak = (exception, timespan) => { Console.WriteLine("CIRCUIT OPEN"); };
-    private static readonly Action OnReset = () => { Console.WriteLine("CIRCUIT RESET"); };
+    internal class ResiliencePolicies
+    {
+        // Break the circuit after the specified number of consecutive exceptions
+        // and keep circuit broken for the specified duration,
+        // calling an action on change of circuit state.
+        private static readonly Action<Exception, TimeSpan> OnBreak = (exception, timespan) => { Console.WriteLine("CIRCUIT OPEN"); };
+        private static readonly Action OnReset = () => { Console.WriteLine("CIRCUIT RESET"); };
 
-    public static readonly Policy CircuitBreaker = Policy
-        .Handle<Exception>()
-        //The circuit will break if the exception is raised once
-        //It will stay broken for 10 seconds, any retries during that time 
-        //will automatically throw a BrokenCircuitException that wraps the exception
-        //that caused it.
-        .CircuitBreakerAsync(1, TimeSpan.FromSeconds(30), OnBreak, OnReset);
-
-    public static readonly Policy ExponentialRetryPolicy =
-        Policy
+        public static readonly Policy CircuitBreaker = Policy
             .Handle<Exception>()
-            .WaitAndRetryAsync(
-                //Retry 3 times
-                3,
-                // using a function to calculate the duration to wait between retries based on 
-                // the current retry attempt (allows for exponential backoff)
-                // In this case will wait for
-                //  2 ^ 1 = 2 seconds then
-                //  2 ^ 2 = 4 seconds then
-                //  2 ^ 3 = 8 seconds
-                attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
-                (ex, _) => Console.WriteLine(ex.ToString()));
+            //The circuit will break if the exception is raised once
+            //It will stay broken for 10 seconds, any retries during that time 
+            //will automatically throw a BrokenCircuitException that wraps the exception
+            //that caused it.
+            .CircuitBreakerAsync(1, TimeSpan.FromSeconds(30), OnBreak, OnReset);
+
+        public static readonly Policy ExponentialRetryPolicy =
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(
+                    //Retry 3 times
+                    3,
+                    // using a function to calculate the duration to wait between retries based on 
+                    // the current retry attempt (allows for exponential backoff)
+                    // In this case will wait for
+                    //  2 ^ 1 = 2 seconds then
+                    //  2 ^ 2 = 4 seconds then
+                    //  2 ^ 3 = 8 seconds
+                    attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
+                    (ex, _) => Console.WriteLine(ex.ToString()));
 
 
-    public static readonly Policy FallbackPolicy =
-        Policy
-            .Handle<Exception>()
-            .FallbackAsync((token) =>
-            {
-                Console.WriteLine("RUN PLAN B");
-                return Task.FromResult(0);
-            });
+        public static readonly Policy FallbackPolicy =
+            Policy
+                .Handle<Exception>()
+                .FallbackAsync((token) =>
+                {
+                    Console.WriteLine("RUN PLAN B");
+                    return Task.FromResult(0);
+                });
 
+    }
 }
--- LibraryCatalog/LibraryStore.cs
+++ LibraryCatalog/LibraryStore.cs
@@ -11,23 +11,23 @@ namespace LibraryCatalog
     {
         public async Task<IEnumerable<LibraryCatalogBook>> GetTopListOfBooks(int numberOfProductsToGet)
         {
-            string ReadItemsSql =
+            string readItemsSql =
                 $"SELECT TOP {numberOfProductsToGet} Id ,Title ,Summary ,SummaryHtml ,Authors ,Url ,SmallImageUrl ,MediumImageUrl ,LargeImageUrl ,Isbn ,Published ,Publisher ,Binding FROM dbo.Book";
 
             using (var conn = new SqlConnection(EnvironmentVariables.ConnectionString))
             {
-                return await conn.QueryAsync<LibraryCatalogBook>(ReadItemsSql);
+                return await conn.QueryAsync<LibraryCatalogBook>(readItemsSql);
             }
         }
 
         public async Task<IEnumerable<LibraryCatalogBook>> GetBooksByIds(IEnumerable<Guid> bookIds)
         {
-            string ReadItemsSql =
+            string readItemsSql =
                 $"SELECT Id ,Title ,Summary FROM dbo.Book WHERE Id in @ids";
 
             using (var conn = new SqlConnection(EnvironmentVariables.ConnectionString))
             {
-                return await conn.QueryAsync<LibraryCatalogBook>(ReadItemsSql, new { ids = bookIds.ToArray()});
+                return await conn.QueryAsync<LibraryCatalogBook>(readItemsSql, new { ids = bookIds.ToArray()});
             }
         }
     }
